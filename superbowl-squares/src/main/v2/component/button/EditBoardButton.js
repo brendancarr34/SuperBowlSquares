7a96e3af4ae9d2edde314310f259ca807c487bb1
Brendan Carr <brendan.c34@gmail.com>
2023-12-01 16:48:07 -0800

    SBS-040: try new GridComponent

--- superbowl-squares/src/main/v2/component/button/EditBoardButton.js
+++ superbowl-squares/src/main/v2/component/button/EditBoardButton.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import { MDBBtn } from "mdb-react-ui-kit";
-import '../../style/Button.css'
+import '../../style/Button.css';
 
 export function EditBoardButton(props) {
-    
-    const id = props.id;
+    const { taken } = props;
     const [active, setActive] = useState(false);
-    const taken = props.taken;
     const [text, setText] = useState("X");
-    
+
     const handleClick = () => {
         if (!taken) {
-            setActive(!active);
-            !active ? setText("🏈") : setText("X");
-            props.isActive(id, !active);
+            setActive(prevActive => {
+                const newActive = !prevActive;
+                newActive ? setText("🏈") : setText("X");
+                // isActive(id, newActive);
+                return newActive;
+            });
         }
     };
 
@@ -24,22 +25,21 @@ export function EditBoardButton(props) {
             </MDBBtn>
         );
     }
-    
+
     return (
         <MDBBtn className="square-md" onClick={handleClick} style={editBoardButtonStyle()}>
-        {/* <MDBBtn className="square-md" onMouseDown={handleClick} style={editBoardButtonStyle()}> */}
             {text}
         </MDBBtn>
     );
 
     function editBoardButtonStyle() {
-        return {  
-            backgroundColor: taken ? "red" : active ? "green" : "white" ,
+        return {
+            backgroundColor: taken ? "red" : active ? "green" : "white",
             color: taken ? "red" : active ? "green" : "white",
             fontSize: 20,
             padding: 0,
             margin: 0,
             border: '1px solid black'
         };
-    } 
+    }
 }
