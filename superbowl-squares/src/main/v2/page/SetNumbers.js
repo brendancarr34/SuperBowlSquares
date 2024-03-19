b8f3cf02eab681d2b1aa34e15cdea6e74263714f
Brendan Carr <bcarr@brendo-pc2.local>
2024-03-19 02:01:21 -0700

    SBS-074: add button for adding random numbers

--- superbowl-squares/src/main/v2/page/SetNumbers.js
+++ superbowl-squares/src/main/v2/page/SetNumbers.js
@@ -13,7 +13,7 @@ import { host } from '../../../config';
 export function SetNumbers() {
     const location = useLocation();
     let navigate = useNavigate();
-    let groupName =  location.state.groupName;
+    let groupName = location.state.groupName;
 
     // State to hold inputsTop and inputsBottom
     const [inputsState, setInputsState] = useState({
@@ -26,17 +26,16 @@ export function SetNumbers() {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://' + host + ':3001/api/game/' + groupName);
-                if (response.data.topNumbers != emptyTopNumbers && response.data.sideNumbers != emptyTopNumbers) {
+                if (response.data.topNumbers !== emptyTopNumbers && response.data.sideNumbers !== emptyTopNumbers) {
                     setInputsState({
-                        inputsTop: response.data.topNumbers, 
+                        inputsTop: response.data.topNumbers,
                         inputsBottom: response.data.sideNumbers
                     });
                 }
+            } catch (error) {
+                console.error('Error fetching data:', error);
             }
-            catch (error) {
-
-            }
-        }
+        };
 
         fetchData();
     }, []);
@@ -50,10 +49,69 @@ export function SetNumbers() {
         setInputsState(inputs);
     };
 
-    // Function to handle button click
+    // Function to handle button click to set random numbers
+    const handleRandomNumbersClick = async () => {
+        const getRandomNumbers = () => {
+            const randomNumbers = [];
+            while (randomNumbers.length < 10) {
+                const randomNumber = Math.floor(Math.random() * 10);
+                if (!randomNumbers.includes(randomNumber)) {
+                    randomNumbers.push(randomNumber);
+                }
+            }
+            return randomNumbers;
+        };
+
+        const randomNumbersTop = getRandomNumbers();
+        const randomNumbersBottom = getRandomNumbers();
+
+        // console.log('randomNumbersTop: ' + randomNumbersTop);
+        // console.log('randomNumbersBottom: ' + randomNumbersBottom);
+
+        // setInputsState({
+        //     inputsTop: randomNumbersTop,
+        //     inputsBottom: randomNumbersBottom
+        // });
+
+        // console.log('inputsState.inputsTop: ' + inputsState.inputsTop);
+        // console.log('inputsState.inputsBottom: ' + inputsState.inputsBottom);
+
+        // setInputsState(prevState => ({
+        //     inputsTop: getRandomNumbers(),
+        //     inputsBottom: getRandomNumbers()
+        // }));
+
+        // console.log('inputsState.inputsTop: ' + inputsState.inputsTop);
+        // console.log('inputsState.inputsBottom: ' + inputsState.inputsBottom);
+
+        try {
+            await axios.post(`http://${host}:3001/api/game/api/setNumbers/${groupName}`,
+                { topNumbers: randomNumbersTop, sideNumbers: randomNumbersBottom });
+
+            navigate('/super-bowl-squares', {
+                replace: true,
+                state: { groupName: groupName }
+            });
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            if (error.response != null) {
+                const errorMessage = error.response.data.message;
+                setErrorMessage(errorMessage);
+                setShowModal(true);
+            } else if (error.code === 'ERR_NETWORK') {
+                setErrorMessage('Network Error');
+                setShowModal(true);
+            } else {
+                setErrorMessage('Unknown Error');
+                setShowModal(true);
+            }
+        }
+    };
+
+    // Function to handle button click to set numbers
     const handleSetNumbersClick = async () => {
         try {
-            await axios.post(`http://${host}:3001/api/game/api/setNumbers/${groupName}`, 
+            await axios.post(`http://${host}:3001/api/game/api/setNumbers/${groupName}`,
                 { topNumbers: inputsState.inputsTop, sideNumbers: inputsState.inputsBottom });
 
             navigate('/super-bowl-squares', {
@@ -80,8 +138,8 @@ export function SetNumbers() {
         navigate('/super-bowl-squares', {
             replace: true,
             state: { groupName: groupName }
-          });
-    }
+        });
+    };
 
     // Function to close the modal
     const handleCloseModal = () => {
@@ -91,22 +149,33 @@ export function SetNumbers() {
     return (
         <Container>
             <Row style={fullHeight()}>
-                <Col style={{'textAlign':'center'}}>
+                <Col style={{ textAlign: 'center' }}>
                     <Row>
                         <Col>
                             <h1>Set Numbers</h1>
                             <h6>Group: {groupName}</h6>
-                            <br/>
-                            <br/>
+                            <br />
+                            <br />
                             {/* Pass handleInputChange function as a prop */}
-                            <NumberInputBoxes onInputChange={handleInputChange} inputsTop={inputsState.inputsTop} inputsBottom={inputsState.inputsBottom}/>
-                            <br/>
+                            <NumberInputBoxes onInputChange={handleInputChange} inputsTop={inputsState.inputsTop} inputsBottom={inputsState.inputsBottom} />
+                            <br />
                         </Col>
                     </Row>
                     <Row>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
+                        <br />
+                    </Row>
+                    <Row>
+                        <Col>
+                            {/* Attach event handler to the button */}
+                            <Button style={blackButton()} onClick={handleRandomNumbersClick}>
+                                Generate Random Numbers
+                            </Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <br />
                     </Row>
                     <Row>
                         <Col>
@@ -117,7 +186,7 @@ export function SetNumbers() {
                         </Col>
                     </Row>
                     <Row>
-                        <br/>
+                        <br />
                     </Row>
                     <Row>
                         <Col>
@@ -142,16 +211,16 @@ export function SetNumbers() {
                 </Modal.Footer>
             </Modal>
         </Container>
-    )
+    );
 }
 
 function fullHeight() {
     return {
-        height:'90vh',
-        display: 'flex', 
-        justifyContent: 'center', 
+        height: '90vh',
+        display: 'flex',
+        justifyContent: 'center',
         alignItems: 'center'
-    }
+    };
 }
 
 function blackButton() {
@@ -159,6 +228,6 @@ function blackButton() {
         backgroundColor: 'black',
         padding: 20,
         border: 'black',
-        width: 155
-    }
+        width: 200
+    };
 }
